4b23ceacd94db9fa8d76b6658829d67bf9a2fd1c
Ahsan Ramadan <ahsanramadan07@gmail.com>
2022-11-18 22:36:42 +0700

    new

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
+import requests
+from flask import Flask, jsonify, redirect, render_template, request, url_for
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://test:asan@cluster0.hmhssac.mongodb.net/?retryWrites=true&w=majority")
 
 app = Flask(__name__)
 
 @app.route("/")
-def pratice():
-    return render_template("prac.html")
+def main():
+    return render_template("index.html")
+
+@app.route("/detail/<keyword>")
+def detail(keyword):
+    return render_template("detail.html",word = keyword)
+
+@app.route("/api/simpan", methods = ["POST"])
+def simpan():
+    return jsonify({'result': 'success', 'msg': 'Kata Tersimpan'})
+
+@app.route("/api/hapus", methods = ["POST"])
+def hapus():
+    return jsonify({'result': 'success', 'msg': 'Kata telah di hapus'})
+    
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000,debug=True)
--- prac.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/nay?key=a222de28-5651-402e-8a5d-222fcd2263ef'
-r = requests.get(url)
-result = r.json()
-print(result)
